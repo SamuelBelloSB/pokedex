755e343f08e87c25d4f116b02116808754b0588d
SamuelBelloSB <samuelbellocontato@gmail.com>
2022-08-25 16:04:04 -0300

    Consegui importar da API o nome e número do pokemon, mas estou com dificuldade de carregar a imagem.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokedex_image');
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+
+renderPokemon('22');
