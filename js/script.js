cf37a9d8638993cdea5a8ef5ec9042e44e1a174c
SamuelBelloSB <samuelbellocontato@gmail.com>
2022-08-25 16:11:33 -0300

    Revisando o código e consegui carregar a imagem, a causa do erro: minha burrice ao escrever pokedex_image ao invés de pokemon_image (o nome correto da class).

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
-const pokemonImage = document.querySelector('.pokedex_image');
+const pokemonImage = document.querySelector('.pokemon_image');
 
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
