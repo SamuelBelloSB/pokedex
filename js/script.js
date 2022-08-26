e73a52bdd512697cd73a0b1ce42a30b502e45dd6
SamuelBelloSB <samuelbellocontato@gmail.com>
2022-08-26 09:10:02 -0300

    Adicionada a função de pesquisa pokémon no formulário. Vi sobre addEventListener.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
     const data = await APIResponse.json();
     return data;
 }
@@ -14,6 +17,10 @@ const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value = '';
 }
 
-renderPokemon('22');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value);
+})
